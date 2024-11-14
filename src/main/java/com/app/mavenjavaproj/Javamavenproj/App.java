ea38bbb899599db72906ac317f2ed5f446c00387
darshinihoney <darshinibachu@gmail.com>
2024-11-14 19:28:00 +0530

    added files

---
+++ src/main/java/com/app/mavenjavaproj/Javamavenproj/App.java
@@ -0,0 +1,13 @@
+package com.app.mavenjavaproj.Javamavenproj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Helloo Darshini! 22BD1A0503" );
+    }
+}
